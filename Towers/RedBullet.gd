6e002d6139cde9a3c1415c47860344611ef43d9b
Course Test <ozaki@cs.sfu.ca>
2023-01-03 13:07:09 -0800

    01-03 stream

--- Towers/RedBullet.gd
+++ Towers/RedBullet.gd
@@ -6,13 +6,30 @@ var pathName
 var currTargets = []
 var curr
 
+var reload = 0
+var range = 400
+
+@onready var timer = get_node("Upgrade/ProgressBar/Timer")
+var startShooting = false
 func _process(delta):
+	get_node("Upgrade/ProgressBar").global_position = self.position + Vector2(-64,-81)
 	if is_instance_valid(curr):
 		self.look_at(curr.global_position)
+		if timer.is_stopped():
+			timer.start()
 	else:
 		for i in get_node("BulletContainer").get_child_count():
 			get_node("BulletContainer").get_child(i).queue_free()
-
+	update_powers()
+func Shoot():
+	
+	var tempBullet = Bullet.instantiate()
+	tempBullet.pathName = pathName
+	tempBullet.bulletDamage = bulletDamage
+	get_node("BulletContainer").add_child(tempBullet)
+	tempBullet.global_position = $Aim.global_position
+	
+	
 func _on_tower_body_entered(body):
 	if "Solider A" in body.name:
 		var tempArray = []
@@ -20,7 +37,7 @@ func _on_tower_body_entered(body):
 		for i in currTargets:
 			if "Solider" in i.name:
 				tempArray.append(i)
-		
+			
 		var currTarget = null
 		for i in tempArray:
 			if currTarget == null:
@@ -28,17 +45,55 @@ func _on_tower_body_entered(body):
 			else:
 				if i.get_parent().get_progress() > currTarget.get_progress():
 					currTarget = i.get_node("../")
-		
+				
 		curr = currTarget
 		pathName = currTarget.get_parent().name
-		
-		var tempBullet = Bullet.instantiate()
-		tempBullet.pathName = pathName
-		tempBullet.bulletDamage = bulletDamage
-		get_node("BulletContainer").add_child(tempBullet)
-		tempBullet.global_position = $Aim.global_position
-		
-		#await get_tree().create_timer(1).timeout
 
 func _on_tower_body_exited(body):
 	currTargets = get_node("Tower").get_overlapping_bodies()
+
+
+
+
+func _on_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.button_mask == 1:
+		var towerPath = get_tree().get_root().get_node("Main/Towers")
+		for i in towerPath.get_child_count():
+			if towerPath.get_child(i).name != self.name:
+				towerPath.get_child(i).get_node("Upgrade/Upgrade").hide()
+		get_node("Upgrade/Upgrade").visible = !get_node("Upgrade/Upgrade").visible
+		get_node("Upgrade/Upgrade").global_position = self.position + Vector2(-572,81)
+		
+
+func _on_range_pressed():
+	range += 30
+	
+func _on_attack_speed_pressed():
+	if reload <= 2:
+		
+		reload += 0.1
+	timer.wait_time = 3 - reload
+	print(reload)
+
+func _on_power_pressed():
+	bulletDamage += 1
+
+
+func _on_timer_timeout():
+	Shoot()
+
+
+func _on_range_mouse_entered():
+	get_node("Tower/CollisionShape2D").show()
+
+
+func _on_range_mouse_exited():
+	get_node("Tower/CollisionShape2D").hide()
+
+
+func update_powers():
+	get_node("Upgrade/Upgrade/HBoxContainer/Range/Label").text = str(range)
+	get_node("Upgrade/Upgrade/HBoxContainer/AttackSpeed/Label").text = str(3 - reload)
+	get_node("Upgrade/Upgrade/HBoxContainer/Power/Label").text = str(bulletDamage)
+	
+	get_node("Tower/CollisionShape2D").shape.radius = range
--- Towers/RedTowerPanel.gd
+++ Towers/RedTowerPanel.gd
@@ -1,24 +1,47 @@
 extends Panel
 
 @onready var tower = preload("res://Towers/RedBullet.tscn")
+var currTile
 
 func _on_gui_input(event):
+	var tempTower = tower.instantiate()
 	if event is InputEventMouseButton and event.button_mask == 1:
-		var tempTower = tower.instantiate()
+		
 		add_child(tempTower)
 		tempTower.global_position = event.global_position
 		tempTower.process_mode = Node.PROCESS_MODE_DISABLED
-	if event is InputEventMouseMotion and event.button_mask == 1:
-		#button left down and dragging
-		get_child(1).global_position = event.global_position
-		get_child(1).scale = Vector2(0.32,0.32)
-	if event is InputEventMouseButton and event.button_mask == 0:
-		#button left release
-		get_child(1).queue_free()
-		print(event.global_position)
-		var tempTower = tower.instantiate()
-		var path = get_tree().get_root().get_node("Main/Towers")
 		
-		path.add_child(tempTower)
-		tempTower.global_position = event.global_position
-		tempTower.get_node("Area").hide()
+		tempTower.scale = Vector2(0.32,0.32)
+	
+	elif event is InputEventMouseMotion and event.button_mask == 1:
+		#button left down and dragging
+		if get_child_count() > 1:
+			
+			get_child(1).global_position = event.global_position
+			#Check if on Dirt Tile.
+			var mapPath = get_tree().get_root().get_node("Main/TileMap")
+			var tile = mapPath.local_to_map(get_global_mouse_position())
+			currTile = mapPath.get_cell_atlas_coords(0, tile, false)
+			
+			if (currTile == Vector2i(4,5)):
+				get_child(1).get_node("Area").modulate = Color(0,255,0)
+			else:
+				get_child(1).get_node("Area").modulate = Color(255,255,255)
+	elif event is InputEventMouseButton and event.button_mask == 0:
+		#button left release
+		if event.global_position.x >= 2944:
+			if get_child_count() > 1:
+				get_child(1).queue_free()
+		else:
+			#check for valid tile:
+			if get_child_count() > 1:
+				get_child(1).queue_free()
+			if currTile == Vector2i(4,5):
+				var path = get_tree().get_root().get_node("Main/Towers")
+				
+				path.add_child(tempTower)
+				tempTower.global_position = event.global_position
+				tempTower.get_node("Area").hide()
+	else:
+		if get_child_count() > 1:
+			get_child(1).queue_free()
